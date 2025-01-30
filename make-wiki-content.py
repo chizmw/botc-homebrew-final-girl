f2bf2c24bde833cf70b43e3bd1f983fdb0f9dc35
Chisel <chisel@malik-wright.uk>
2025-01-30 13:53:58 +0000

    feat: use jinja2 template to render Abilities

--- make-wiki-content.py
+++ make-wiki-content.py
@@ -27,47 +27,16 @@ with open(source_json) as f:
             "name": item["name"],
             "ability": item["ability"],
             "id": item["id"],
+            "primary_image": item["image"][0],
         }
         results[item["team"]].append(character_info)
         # print(results)
 
+template = env.get_template("abilities.jinja.md")
+rendered = template.render(teams=results)
 with open(destination_markdown, "w") as f:
-    image_base = f"https://raw.githubusercontent.com/{repo_slug}/refs/heads/main/images/character/"
-
-    # get each team
-    for team in results:
-        # sentence case the team name
-        team_header = team[0].upper() + team[1:]
-        f.write(f"## {team_header}\n\n")
-        # get each player
-        for character in results[team]:
-            # print(character)
-            f.write(f"### {character["name"]}\n\n")
-
-            team_letter = ""
-            # townfolk and outsider -> "G"
-            if team == "townsfolk" or team == "outsider":
-                team_letter = "G"
-            # minion and demon -> "E"
-            elif team == "minion" or team == "demon":
-                team_letter = "E"
-
-            ability: str = character["ability"]
-            image_url = f"{image_base}{character["id"]}_{team_letter}.png"
-
-            # print(f"{image_url} {ability}")
-
-            # f.write(f"{results[team][character]}\n\n")
-
-            # write a one row html table
-            f.write(
-                f"""<table border="0" width="100%">
-                <tr>
-                    <td><img src="{image_url}" alt="{character["name"]}" height="80"></td>
-                    <td>{ability}</td>
-                </tr>
-            </table>\n\n"""
-            )
+    f.write(rendered)
+    f.write("\n\n")
 
 for id in characters:
     character = characters[id]
---
+++ pyapp/templates/abilities.jinja.md
@@ -0,0 +1,25 @@
+# Abilities
+
+<!-- markdownlint-disable-file MD013 -->
+<!-- markdownlint-disable-file MD033 -->
+
+This is a summary of the characters and their abilities
+
+{%- for team in teams -%}
+
+## {{ team |title }}
+
+{%- set members = teams[team] -%}
+{%- for member in members %}
+
+### {{member.name}}
+
+<table border="0" width="100%">
+    <tr>
+        <td><img src="{{member.primary_image}}" alt="{{member.name}}" height="80"></td>
+        <td>{{member.ability}}</td>
+    </tr>
+</table>
+{%- endfor -%}
+
+{%- endfor -%}
